a85af6ff2ceb21f79e7a41e5dddc45fe1080bb26
Codemanja <codemanjaa@gmail.com>
2017-04-04 17:32:44 +0300

    Update Backup.sql

--- RECOVERY/Backup.sql
+++ RECOVERY/Backup.sql
@@ -1,4 +1,4 @@
-USE RestTest
+UUSE RestTest
 
 CREATE TABLE Test(
 id INT NOT NULL PRIMARY KEY,
@@ -6,20 +6,20 @@ name VARCHAR(20) NOT NULL
 )
 
 --FulBackup
-BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\resttest_full_t1.bak'
+BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_full_t1.bak'
 --Insert two new rows
 
 INSERT INTO Test (id, name) VALUES (1, 'T1 insert 1');
 INSERT INTO Test (id, name) VALUES (2, 'T1 Insert 2');
 
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\rt_log_t1.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t1.bak'
 
 -- Step 8
 INSERT INTO Test (id, name) VALUES (3, 'T1 insert 3');
 INSERT INTO Test (id, name) VALUES (4, 'T1 Insert 4');
 
 -- Take a log back up
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\rt_log_t2.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t2.bak'
 
 -- Insert two rows
 
@@ -27,7 +27,7 @@ INSERT INTO Test (id, name) VALUES (5, 'T1 insert 5');
 INSERT INTO Test (id, name) VALUES (6, 'T1 Insert 6');
 
 --Take a differential backup
-BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\rt_dlog_t3.bak' WITH DIFFERENTIAL
+BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_differential_t3.bak' WITH DIFFERENTIAL
 
 --insert two new rows
 
@@ -37,13 +37,13 @@ INSERT INTO Test (id, name) VALUES (8, 'T1 Insert 8');
 
 
 --Take a database backup
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\rt_log_t4.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t4.bak'
 
 
 --
 INSERT INTO Test (id, name) VALUES (9, 'T1 insert 9')
 
 --
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\rt_log_t5.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t5.bak'
 --
 INSERT INTO Test (id, name) VALUES (10, 'T1 Insert 10')
