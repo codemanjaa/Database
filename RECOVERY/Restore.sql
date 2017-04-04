6ab6919d4c24fdae27d0ff17585f778f1912ae8b
Codemanja <codemanjaa@gmail.com>
2017-04-04 20:04:21 +0100

    Update Restore.sql

--- RECOVERY/Restore.sql
+++ RECOVERY/Restore.sql
@@ -1,24 +1,41 @@
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t5.bak' WITH CONTINUE_AFTER_ERROR
+BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t7.bak' WITH CONTINUE_AFTER_ERROR
+
+
 
 --Start from the last full backup
 
-RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\RestTest_full_t1.bak' WITH NORECOVERY
+
+
+RESTORE DATABASE RestTest FROM DISK = 'E:\DBP_backups\RestTest_full_t1.bak' WITH NORECOVERY
+
+
 
 -- RESTORE the last differential back up 
 
-RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\RestTest_differential_t3.bak'
+
+
+RESTORE DATABASE RestTest FROM DISK = 'E:\DBP_backups\RestTest_differential_t4.bak'
+
+
 
 -- Restore the log backups
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\RestTest_log_t1.bak' WITH NORECOVERY
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\RestTest_log_t2.bak' WITH NORECOVERY
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\RestTest_log_t4.bak' WITH NORECOVERY
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\RestTest_log_t5.bak' WITH NORECOVERY
 
+--RESTORE LOG RestTest FROM DISK = 'E:\DBP_backups\RestTest_log_t2.bak' WITH NORECOVERY
+
+--RESTORE LOG RestTest FROM DISK = 'E:\DBP_backups\RestTest_log_t3.bak' WITH NORECOVERY
+
+RESTORE LOG RestTest FROM DISK = 'E:\DBP_backups\RestTest_log_t5.bak' WITH NORECOVERY
+
+RESTORE LOG RestTest FROM DISK = 'E:\DBP_backups\RestTest_log_t6.bak' WITH NORECOVERY
 
 -- Completion
 
+
+
 RESTORE DATABASE RestTest WITH RECOVERY
 
-USE RestTest
-SELECT * FROM Test
 
+
+USE RestTest
+
+SELECT * FROM Test
