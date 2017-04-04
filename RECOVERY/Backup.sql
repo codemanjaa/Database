6a1912c9735c358eeec201d460f69347da4a4373
Codemanja <codemanjaa@gmail.com>
2017-04-04 19:52:51 +0100

    Update Backup.sql

--- RECOVERY/Backup.sql
+++ RECOVERY/Backup.sql
@@ -1,49 +1,103 @@
+CREATE DATABASE RestTest
+
+
+
+
+
 USE RestTest
 
+
+
 CREATE TABLE Test(
+
 id INT NOT NULL PRIMARY KEY,
+
 name VARCHAR(20) NOT NULL
+
 )
 
---FulBackup
-BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_full_t1.bak'
+ALTER DATABASE RestTest SET RECOVERY FULL
+
+--FullBackup
+
+BACKUP DATABASE RestTest TO DISK = 'E:\DBP_backups\RestTest_full_t1.bak'
+
 --Insert two new rows
 
+
+
 INSERT INTO Test (id, name) VALUES (1, 'T1 insert 1');
+
 INSERT INTO Test (id, name) VALUES (2, 'T1 Insert 2');
 
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t1.bak'
+
+
+BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t2.bak'
+
+
 
 -- Step 8
+
 INSERT INTO Test (id, name) VALUES (3, 'T1 insert 3');
+
 INSERT INTO Test (id, name) VALUES (4, 'T1 Insert 4');
 
+
+
 -- Take a log back up
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t2.bak'
+
+BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t3.bak'
+
+
 
 -- Insert two rows
 
+
+
 INSERT INTO Test (id, name) VALUES (5, 'T1 insert 5');
+
 INSERT INTO Test (id, name) VALUES (6, 'T1 Insert 6');
 
+
+
 --Take a differential backup
-BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_differential_t3.bak' WITH DIFFERENTIAL
+
+BACKUP DATABASE RestTest TO DISK = 'E:\DBP_backups\RestTest_differential_t4.bak' WITH DIFFERENTIAL
+
+
 
 --insert two new rows
 
 
+
+
+
 INSERT INTO Test (id, name) VALUES (7, 'T1 insert 7');
+
 INSERT INTO Test (id, name) VALUES (8, 'T1 Insert 8');
 
 
+
+
+
 --Take a database backup
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t4.bak'
+
+BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t5.bak'
+
+
+
 
 
 --
+
 INSERT INTO Test (id, name) VALUES (9, 'T1 insert 9')
 
+
+
 --
-BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t5.bak'
+
+BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t6.bak'
+
 --
+
 INSERT INTO Test (id, name) VALUES (10, 'T1 Insert 10')
