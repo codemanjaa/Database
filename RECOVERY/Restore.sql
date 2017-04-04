40785e31e5719318ca322d81ed49fac0715c4986
Codemanja <codemanjaa@gmail.com>
2017-04-04 17:01:13 +0300

    Update Restore.sql

--- RECOVERY/Restore.sql
+++ RECOVERY/Restore.sql
@@ -1,19 +1,18 @@
---Create tail-log backup
-
 BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\backuplog.bak' WITH CONTINUE_AFTER_ERROR
 
 --Start from the last full backup
 
-RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\backuplog.bak' WITH NORECOVERY
+RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\resttest_full_t1.bak' WITH NORECOVERY
 
 -- RESTORE the last differential back up 
 
-RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\backuplog.bak'
+RESTORE DATABASE RestTest FROM DISK = 'C:\DBP_backups\rt_dlog_t3.bak'
 
 -- Restore the log backups
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\backuplog.bak' WITH NORECOVERY
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\backuplog.bak' WITH NORECOVERY
-RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\backuplog.bak' WITH NORECOVERY
+RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\rt_logt1.bak' WITH NORECOVERY
+RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\rt_log_t2.bak' WITH NORECOVERY
+RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\rt_log_t4.bak' WITH NORECOVERY
+RESTORE LOG RestTest FROM DISK = 'C:\DBP_backups\rt_log_t5.bak' WITH NORECOVERY
 
 
 -- Completion
