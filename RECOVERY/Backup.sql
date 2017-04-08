e184a38a753232e397e38503a5ec2cea75b15110
Codemanja <codemanjaa@gmail.com>
2017-04-08 14:13:16 +0300

    Update Backup.sql

--- RECOVERY/Backup.sql
+++ RECOVERY/Backup.sql
@@ -6,7 +6,7 @@ CREATE DATABASE RestTest
 
 USE RestTest
 
-
+ALTER DATABASE RestTest SET RECOVERY FULL
 
 CREATE TABLE Test(
 
@@ -20,7 +20,7 @@ ALTER DATABASE RestTest SET RECOVERY FULL
 
 --FullBackup
 
-BACKUP DATABASE RestTest TO DISK = 'E:\DBP_backups\RestTest_full_t1.bak'
+BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_full_t1.bak'
 
 --Insert two new rows
 
@@ -32,21 +32,21 @@ INSERT INTO Test (id, name) VALUES (2, 'T1 Insert 2');
 
 
 
-BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t2.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t2.bak'
 
 
 
 -- Step 8
 
-INSERT INTO Test (id, name) VALUES (3, 'T1 insert 3');
+INSERT INTO Test (id, name) VALUES (3, 'T2 insert 3');
 
-INSERT INTO Test (id, name) VALUES (4, 'T1 Insert 4');
+INSERT INTO Test (id, name) VALUES (4, 'T2 Insert 4');
 
 
 
 -- Take a log back up
 
-BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t3.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t3.bak'
 
 
 
@@ -62,7 +62,7 @@ INSERT INTO Test (id, name) VALUES (6, 'T1 Insert 6');
 
 --Take a differential backup
 
-BACKUP DATABASE RestTest TO DISK = 'E:\DBP_backups\RestTest_differential_t4.bak' WITH DIFFERENTIAL
+BACKUP DATABASE RestTest TO DISK = 'C:\DBP_backups\RestTest_differential_t4.bak' WITH DIFFERENTIAL
 
 
 
@@ -82,22 +82,17 @@ INSERT INTO Test (id, name) VALUES (8, 'T1 Insert 8');
 
 --Take a database backup
 
-BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t5.bak'
-
-
-
-
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t5.bak'
 
 --
 
 INSERT INTO Test (id, name) VALUES (9, 'T1 insert 9')
 
-
-
 --
 
-BACKUP LOG RestTest TO DISK = 'E:\DBP_backups\RestTest_log_t6.bak'
+BACKUP LOG RestTest TO DISK = 'C:\DBP_backups\RestTest_log_t6.bak'
 
 --
 
 INSERT INTO Test (id, name) VALUES (10, 'T1 Insert 10')
+
