ae8f6e522fcb77cab2d48f3578dba910a8e52fd4
Codemanja <codemanjaa@gmail.com>
2017-04-11 15:50:17 +0300

    Update InsertTrigger.sql

--- TRIGGER/InsertTrigger.sql
+++ TRIGGER/InsertTrigger.sql
@@ -23,7 +23,7 @@ new_balance DECIMAL(11,2)
 
  
 
-CREATE TRIGGER Account_Insert_Trigger ON Account
+ALTER TRIGGER Account_Insert_Trigger ON Account
 AFTER INSERT
 AS
 BEGIN
@@ -36,12 +36,44 @@ SELECT @username = spid FROM master.dbo.sysprocesses  WHERE spid = @@SPID
 
 SELECT @account_number = INSERTED.account_number, @balance = INSERTED.balance
 FROM INSERTED
-INSERT INTO History (account_number,modification_date, modification_time, username, modification_type, old_balance, new_balance) VALUES
-(@account_number, CONVERT(date, GETDATE()), CONVERT(time, CURRENT_TIMESTAMP), @username, 'INSERT', null, null)
+INSERT INTO History  VALUES
+(@account_number, CONVERT(date, GETDATE()), CONVERT(time, CURRENT_TIMESTAMP), @username, 'INSERT', @balance, @balance)
 
 END
 
 print 'INSERT Success'
-INSERT INTO Account (account_number,balance) VALUES(2, 15000.00)
 
+INSERT INTO Account (account_number,balance) VALUES(10, 15000.00)
+
+SELECT * FROM History
+
+
+ALTER TRIGGER Account_Update_balance_Trigger ON Account
+AFTER UPDATE
+AS
+	IF UPDATE(balance)
+
+		BEGIN
+			SELECT old.balance AS 'Old Balance',
+				   new.balance AS 'New balance'
+			FROM DELETED AS old
+				 JOIN INSERTED AS new ON (new.account_number = old.account_number)
+
+--			SELECT @oldbalance = DELETED.balance, @newbalance = DELETED.new_balance FROM DELETED
+		UPDATE History SET old_balance = old.balance, new_balance = old.balance FROM DELETED as old JOIN History AS new ON
+		(new.account_number = old.account_number)
+				 
+				 
+print 'Update balance success'			 
+END
+
+
+-- UPDATE THE History table
+
+
+
+
+UPDATE Account set balance = 50000.00 WHERE account_number = 2
+
+SELECT * FROM Account
 SELECT * FROM History
